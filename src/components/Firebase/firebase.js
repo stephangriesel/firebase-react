2106188b8d1b6a5432d1da6aecd9bffe0616e5e9
Stephan Griesel <sgriesel@gmail.com>
2018-12-16 09:53:32 +0100

    finish up authethentication interface

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -21,6 +21,16 @@ var config = {
   doCreateUserWithEmailAndPassword = (email,password) =>
     this.auth.createUserWithEmailAndPassword(email,password);
 
+  doSignInWithEmailAndPassword = (email,password) =>
+    this.auth.signInWithEmailAndPassword(email,password);
+  
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+
   }
 
   export default Firebase;
