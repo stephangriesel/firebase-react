2700e8b6b889010ecd2377e42ea9a8fcc64abc7a
Stephan Griesel <sgriesel@gmail.com>
2018-12-16 07:53:26 +0100

    route component mapping

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../Signup';
+import SignInPage from '../Signin';
+import PasswordForgetPage from '../PasswordForgetPage';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 import { ThemeProvider } from "styled-components";
 
@@ -17,7 +29,17 @@ const App = () => (
   <ThemeProvider theme={theme}>
     <Wrapper>
       <Router>
+        <div>
           <Navigation />
+          <hr />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
       </Router>
     </Wrapper>
   </ThemeProvider>
