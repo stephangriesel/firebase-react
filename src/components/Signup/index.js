ef0434634ea53062928c09c3291dafb9d83c8509
Stephan Griesel <sgriesel@gmail.com>
2018-12-18 05:54:12 +0100

    signup component update

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Signup = () => (
+import * as ROUTES from '../../constants/routes';
+
+const SignupPage = () => (
     <div>
         <h1>Sign Up</h1>
+        <SignUpForm />
     </div>
 );
 
-export default Signup;
+const INITIAL_STATE = {
+    username:'',
+    email:'',
+    passwordOne:'',
+    passwordTwo:'',
+    error:null,
+};
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state
+    }
+
+    onSubmit = event => {
+
+    }
+
+    onChange = event => {
+
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+            
+            </form>
+        );
+    }
+}
+
+const SignUpLink = () = (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
+export default SignupPage;
+
+export {SignUpForm, SignUpLink};
